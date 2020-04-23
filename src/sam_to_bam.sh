d1fcc11189af0a63d1e920fe23a4f79882abd451
Ubuntu <ubuntu@ip-172-31-12-219.eu-central-1.compute.internal>
2020-04-23 09:51:35 +0000

    first commit

---
+++ src/sam_to_bam.sh
@@ -0,0 +1,6 @@
+samtools sort -@ 8 -o $RNA_ALIGN_DIR/UHR_Rep1.bam $RNA_ALIGN_DIR/UHR_Rep1.sam
+samtools sort -@ 8 -o $RNA_ALIGN_DIR/UHR_Rep2.bam $RNA_ALIGN_DIR/UHR_Rep2.sam
+samtools sort -@ 8 -o $RNA_ALIGN_DIR/UHR_Rep3.bam $RNA_ALIGN_DIR/UHR_Rep3.sam
+samtools sort -@ 8 -o $RNA_ALIGN_DIR/HBR_Rep1.bam $RNA_ALIGN_DIR/HBR_Rep1.sam
+samtools sort -@ 8 -o $RNA_ALIGN_DIR/HBR_Rep2.bam $RNA_ALIGN_DIR/HBR_Rep2.sam
+samtools sort -@ 8 -o $RNA_ALIGN_DIR/HBR_Rep3.bam $RNA_ALIGN_DIR/HBR_Rep3.sam
